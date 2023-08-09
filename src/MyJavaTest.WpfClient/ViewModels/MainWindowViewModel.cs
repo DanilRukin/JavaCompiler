f95404b75d8c6807e3f30257a76418e963cc9aff
Rukin Danil <rukin2018d@gmail.com>
2023-08-09 21:48:18 +0700

    Создание интерфейса

---
+++ src/MyJavaTest.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MyJavaTest.WpfClient.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Markup;
+
+namespace MyJavaTest.WpfClient.ViewModels
+{
+    [MarkupExtensionReturnType(typeof(MainWindowViewModel))]
+    public class MainWindowViewModel : ViewModel
+    {
+        #region Properties
+        private string _title = "MyJavaTest";
+        public string Title { get => _title; set => Set(ref _title, value); }
+
+        private string _status = "Status";
+        public string Status { get => _status; set => Set(ref _status, value); }
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
